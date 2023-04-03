d44af734bdc52c681f8a38e682228fc9a5ef8472
Cloud User <ec2-user@ip-172-31-87-182.ec2.internal>
2023-04-03 12:30:40 +0000

    d

--- big.c
+++ big.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+biggest() {
 
   double n1, n2, n3;
 
@@ -19,5 +19,5 @@ int main() {
   if (n3 >= n1 && n3 >= n2)
     printf("%.2f is the largest number.", n3);
 
-  return 0;
+ // return 0;
 }
